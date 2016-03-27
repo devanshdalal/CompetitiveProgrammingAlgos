cb210ebc4775dc7ccc48206d2020b714934b1565
Devansh <mr.devanshdalal@gmail.com>
2016-03-28 01:22:26 +0530

    devanshdalal/codechef/RETPO/RETPO-4190179.cpp ADDED

---
+++ devanshdalal/codechef/RETPO/RETPO-4190179.cpp
@@ -0,0 +1,60 @@
+#include<stdio.h>
+#include<string.h>
+#include<iostream>
+#include<algorithm>
+#include<cmath>
+#include<cstdlib>
+#include<queue>
+#include<map>
+#define DD ios_base::sync_with_stdio(false)
+#define maxx 10000001
+#define PI 3.14159265358979323846264338327950288419716939937510
+#define eps 0.0000001
+typedef long long ll;
+typedef unsigned long long ul;
+typedef double d;
+using namespace std;
+
+#ifdef _WIN32
+inline int getchar_unlocked() { return getchar(); }
+inline int putchar_unlocked(char ch) { return putchar(ch);  }
+#endif
+void r(int &x){
+    register unsigned int c = getchar_unlocked();
+    x = 0;int neg=0;
+    for(;(c<48 || c>57) && c!='-';c = getchar_unlocked());
+    if(c=='-'){neg=1; c=getchar_unlocked(); }
+    for(;c>47 && c<58;c = getchar_unlocked() ) {x = (x<<1) + (x<<3) + c - 48;}
+    if(neg)x=-x;
+}
+
+char buf[11];int ix; 
+inline void writeInt(int x) {
+     ix = 10;
+     buf[10] = '\0';
+     do 
+     {
+        buf[--ix] = (x % 10) + '0';
+        x/= 10;
+     }while(x);
+     do
+     {
+        putchar_unlocked(buf[ix]);
+     } while (buf[++ix] != '\0');
+}
+
+inline ll find(ll x,ll y){
+	if (x>=y)
+		return ((x-y)<<1)+((x-y)&1)+ (y<<1);
+	return ((y-x)<<1)-((y-x)&1)+ (x<<1);
+}
+
+int main(){
+    int t,i,j,k,l,n,m,p,q;
+    scanf("%d",&t);
+    while(t--){
+        scanf("%d%d",&n,&m);
+        printf("%lld\n", find((ll)abs(n),(ll)abs(m)) );
+    }
+    return 0;
+}
