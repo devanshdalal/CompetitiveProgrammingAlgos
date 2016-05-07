65cb64fd5326ae3b0f3f0af9de2bde6964c7bf62
Devansh <mr.devanshdalal@gmail.com>
2016-05-07 16:45:49 +0530

    devanshdalal/codeforces/431-A/431-A-6670731.cpp ADDED

---
+++ devanshdalal/codeforces/431-A/431-A-6670731.cpp
@@ -0,0 +1,66 @@
+#include<stdio.h>
+#include<string.h>
+#include<iostream>
+#include<algorithm>
+#include<map>
+#include<cmath>
+#include<bitset>
+#include<vector>
+#include<map>
+#include<stack>
+// #define DD ios_base::sync_with_stdio(false)
+#define maxx 10001
+using namespace std;
+typedef long long ll;
+
+// #ifdef _WIN32
+// inline int getchar_unlocked() { return getchar(); }
+// inline putchar_unlocked(char ch) { return putchar(ch);  }
+// #endif
+// void r(int &x){
+//     register unsigned int c = getchar_unlocked();
+//     x = 0;int neg=0;
+//     for(;(c<48 || c>57) && c!='-';c = getchar_unlocked());
+//     if(c=='-'){neg=1; c=getchar_unlocked(); }
+//     for(;c>47 && c<58;c = getchar_unlocked() ) {x = (x<<1) + (x<<3) + c - 48;}
+//     if(neg)x=-x;
+// }
+
+// char buf[11];int ix; 
+// inline void writeInt(int x) {
+//      ix = 10;
+//      buf[10] = '\0';
+//      do 
+//      {
+//         buf[--ix] = (x % 10) + '0';
+//         x/= 10;
+//      }while(x);
+//      do
+//      {
+//         putchar_unlocked(buf[ix]);
+//      } while (buf[++ix] != '\0');
+// }
+
+
+int main(){
+	register ll t,l,n,m,p,q,b;
+	int i,j;
+	cin >> n>>m>>p>>q;
+	// printf("%lld %lld%lld\n",n,m,p );
+	char * a = new char[200000];
+	scanf("%s",a);
+	l=strlen(a);
+	// cout << n <<" "<< m <<" "<<p<<" "<<q<<" " << l << endl;
+	ll ans =0;
+	for (i = 0; i < l; ++i)
+	 {
+	 	if(a[i]=='1')ans=ans+n;
+	 	else if(a[i]=='2')ans=ans+m;
+	 	else if(a[i]=='3')ans=ans+p;
+	 	else ans=ans+q;
+	 	// cout << ans << " ";
+	 }
+	 cout<< ans << endl;
+
+	return 0;
+}
