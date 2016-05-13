1d2fadc679ba56abe914e1859920bf2dbd28eaff
Devansh <mr.devanshdalal@gmail.com>
2016-05-13 11:45:38 +0530

    dddlll/codechef/PRGIFT/PRGIFT-4419541.cpp ADDED

---
+++ dddlll/codechef/PRGIFT/PRGIFT-4419541.cpp
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<string.h>
+#include<iostream>
+#include<algorithm>
+#include<cmath>
+#include<cstdlib>
+#include<queue>
+#include<map>
+#define DD ios_base::sync_with_stdio(false)
+#define eps 0.0000001
+typedef long long ll;
+typedef unsigned long long ul;
+typedef double d;
+using namespace std;
+const ll mod = 1000000007;
+
+int main(){
+    int t,i,j,k,l,n,m,p,q;
+    scanf("%d",&t);
+    while(t--){
+    	p=0;
+        scanf("%d%d",&n,&k);
+        for (i = 0; i < n; ++i){
+        	scanf("%d",&q);
+        	if(q%2==0)p++;
+        }
+
+        
+        printf(p>=k and not(k==0 and p==n) ? "YES\n":"NO\n");
+
+    }
+    return 0;
+}
