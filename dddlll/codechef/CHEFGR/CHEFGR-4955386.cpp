0ae9242ac101294134124f8e80007f2ebb1962a9
Devansh <mr.devanshdalal@gmail.com>
2016-05-14 23:55:19 +0530

    dddlll/codechef/CHEFGR/CHEFGR-4955386.cpp ADDED

---
+++ dddlll/codechef/CHEFGR/CHEFGR-4955386.cpp
@@ -0,0 +1,66 @@
+#include<stdio.h>
+#include<string.h>
+#include<iostream>
+#include<algorithm>
+#include<cmath>
+#include<cstdlib>
+#include<queue>
+#include<map>
+#define DD ios_base::sync_with_stdio(false)
+#define maxx 10000001
+#define PI 3.14159265358979323846264338327950288419716939937510
+#define eps 0.0000001
+typedef long long ll;
+typedef unsigned long long ul;
+typedef double d;
+using namespace std;
+const ll mod = 1000000007;
+
+#ifdef _WIN32
+inline int getchar_unlocked() { return getchar(); }
+inline int putchar_unlocked(char ch) { return putchar(ch);  }
+#endif
+void r(int &x){
+    register unsigned int c = getchar_unlocked();
+    x = 0;
+    for(;(c<48 || c>57);c = getchar_unlocked());
+    for(;c>47 && c<58;c = getchar_unlocked() ) {x = (x<<1) + (x<<3) + c - 48;}
+}
+
+char buf[11];int ix; 
+inline void writeInt(int x) {
+     ix = 10;
+     buf[10] = '\n';
+     do 
+     {
+        buf[--ix] = (x % 10) + '0';
+        x/= 10;
+     }while(x);
+     do
+     {
+        putchar_unlocked(buf[ix]);
+     } while (buf[++ix] != '\0');
+}
+
+int main(){
+    int t,i,j,k,l,n,m,p,q;
+    scanf("%d",&t);
+    int a[101];
+    while(t--){
+        scanf("%d %d",&n,&m);
+        p=0;
+        for ( i = 0; i < n; ++i)
+        {
+            scanf("%d",&a[i]);
+            p=max(p,a[i]);
+        }
+        q=0;
+        for (i = 0; i < n; ++i)
+        {
+            q+=(p-a[i]);
+        }
+
+        printf( (m-q)%n==0 ?"Yes\n":"No\n");
+    }
+    return 0;
+}
