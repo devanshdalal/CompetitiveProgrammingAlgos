6d8beee026fdcfd0574b9da60217af9a92a3c4b3
Devansh <mr.devanshdalal@gmail.com>
2016-05-20 11:20:00 +0530

    dddlll/codechef/RRMTRX2/RRMTRX2-5628236.cpp added

---
+++ dddlll/codechef/RRMTRX2/RRMTRX2-5628236.cpp
@@ -0,0 +1,87 @@
+#include <bits/stdc++.h>
+#define mp make_pair
+#define pb push_back
+#define VI vector <int>
+#define VVI vector < 	vi >
+#define PII pair <int,int>
+#define VPII vector < pii >
+#define MI map < int, int >
+#define SI set< int >
+#define clr(x) x.clear()
+#define sz(x) ((int) x.size())
+#define F first
+#define S second
+#define abs(a) ((a<0)?(-a):a)
+#define max(a,b) ((a>b)?a:b)
+#define min(a,b) ((a<b)?a:b)
+#define mem1(a,c,b) (memset(a, c, sizeof(a[0]) * b))
+#define mem2(a,d,b,c) (memset(a, d, sizeof(a[0][0])*b*c))
+#define fastio ios_base::sync_with_stdio(0)
+#define dbg(x) {cerr << #x << " = " << x << endl;}
+typedef long long ll;
+typedef unsigned long long ull;
+typedef double dbl;
+using namespace std;
+ll mod = 10000007;
+
+inline ll mpow(ll b, ll ex){
+	if (b==1)return 1;
+    ll r = 1;
+    while (ex ){
+        if (ex&1)r=(r * b)%mod;
+        ex = ex >> 1;
+        b = (b * b)%mod;}
+    return r;
+}
+
+int L[3001][3001];  // Create a table to store results of subproblems 
+
+void lps(char *str)
+{
+   int n = strlen(str);
+   int i, j, cl;
+   for (i = 0; i < n; i++)
+      L[i][i] = 1;
+    for (cl=2; cl<=n; cl++)
+    {
+        for (i=0; i<n-cl+1; i++)
+        {
+            j = i+cl-1;
+            if (str[i] == str[j] && cl == 2)
+               L[i][j] = 2;
+            else if (str[i] == str[j])
+               L[i][j] = L[i+1][j-1] + 2;
+            else
+               L[i][j] = max(L[i][j-1], L[i+1][j]);
+        }
+    }
+    // return L[0][n-1];
+}
+
+int main(){
+    fastio;
+    	
+    ll t,n,p,q,i,m,j;
+    	cin >> n >> m ;
+    	int *a=new int[m+1];
+    	for (j = 1; j <=m; ++j)
+    		{
+	    		a[j]=0;
+    		}
+    	for (i = 1; i <=n; ++i)
+    	{
+    		for (j = 1; j <=m; ++j)
+    		{
+	    		cin >> p;
+	    		a[j]+=p;
+    		}
+    	}
+    	ll ans=1;
+    	for (i = 1; i <=m; ++i)
+    	{
+    		ans=ans*ll(mod+a[i])%mod;
+    	}
+    	cout << (ans+mod)%mod << endl;
+
+    return 0;
+}
